1a0482016e9090475c863e1938d48c2ac614a161
code-with-lakshmi <lavanyameganathan2006@gmail.com>
2026-07-12 13:55:18 +0530

    updated

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ if not api_key:
 
 genai.configure(api_key=api_key)
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 st.set_page_config(
     page_title="AI Learning Buddy Lakshmi",
--- check_models.py
+++ check_models.py
@@ -1,12 +1,6 @@
-import os
 import google.generativeai as genai
 
-api_key = os.getenv("GOOGLE_API_KEY")
-
-if not api_key:
-    raise ValueError("GOOGLE_API_KEY environment variable is not set.")
-
-genai.configure(api_key=api_key)
+genai.configure(api_key="YOUR_API_KEY")
 
 for m in genai.list_models():
     if "generateContent" in m.supported_generation_methods:
